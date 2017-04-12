b9330aa1141fd6c5a30971eb6f16c0e59760718f
Bobby <bobby@sankhagowit.com>
2017-04-11 23:07:12 -0500

    Initial Commit sql schema file

---
+++ tournament.sql
@@ -0,0 +1,18 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+--
+--
+
+-- player table containing id, name, wins initialized at zero and matches
+-- initialized at zero.
+CREATE TABLE players (
+    id serial primary key,
+    name text
+);
+
+CREATE TABLE results (
+    
+);
