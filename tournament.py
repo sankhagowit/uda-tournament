d65f5bc2e123fd9a534c70fe516491e01af59003
Bobby Laptop <bobby@sankhagowit.com>
2017-04-26 00:10:26 -0500

    feat: Add all required functionality

--- tournament.py
+++ tournament.py
@@ -15,7 +15,7 @@ def deleteMatches():
     """Remove all the match records from the database."""
     db = connect()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM matches")
+    cursor.execute("UPDATE players SET wins = 0, matches = 0;")
     db.commit() # Do I need this for the delete?
     cursor.close() # All the examples have this line in addition to db.close()
     db.close()
@@ -25,7 +25,7 @@ def deletePlayers():
     """Remove all the player records from the database."""
     db = connect()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM players")
+    cursor.execute("DELETE FROM players;")
     db.commit() # Do I need this for the delete?
     cursor.close() # All the examples have this line in addition to db.close()
     db.close()
@@ -33,7 +33,13 @@ def deletePlayers():
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    # SELECT count(*) FROM players;
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT count(*) FROM players;")
+    output = cursor.fetchone()
+    cursor.close()
+    db.close()
+    return output[0]
 
 
 def registerPlayer(name):
@@ -48,7 +54,7 @@ def registerPlayer(name):
     db = connect()
     cursor = db.cursor()
     clean_name = bleach.clean(name)
-    cursor.execute("INSERT INTO players (name) VALUES (%s)", (clean_name))
+    cursor.execute("INSERT INTO players (name) VALUES (%s);", (clean_name,))
     db.commit()
     db.close()
 
@@ -66,6 +72,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM players ORDER BY wins DESC;""")
+    output = cursor.fetchall()
+    db.close()
+    return output
 
 
 def reportMatch(winner, loser):
@@ -75,6 +87,19 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM players WHERE id = %s OR id = %s;", (winner, loser))
+    output = cursor.fetchall() # wins index 2, matches index 3
+    winnerWins = output[0][2] + 1
+    winnerMatches = output[0][3] + 1
+    loserMatches = output[1][3] + 1
+
+    cursor.execute("UPDATE players SET wins = %s, matches = %s WHERE id = %s;", (winnerWins, winnerMatches, winner))
+    db.commit()
+    cursor.execute("UPDATE players SET matches = %s WHERE id = %s;", (loserMatches, loser))
+    db.commit()
+    db.close()
 
 
 def swissPairings():
@@ -92,3 +117,13 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name FROM players ORDER BY wins DESC")
+    output = cursor.fetchall()
+    matches = len(output)/2
+    swissPairings = []
+    for x in range(0, matches):
+        pairing = (output[2*x][0], output[2*x][1], output[2*x+1][0], output[2*x+1][1])
+        swissPairings.append(pairing)
+    return swissPairings
--- tournament.sql
+++ tournament.sql
@@ -2,20 +2,17 @@
 --
 -- Put your SQL 'create table' statements in this file; also 'create view'
 -- statements if you choose to use it.
-
 -- player table containing id, name, wins initialized at zero and matches
 -- initialized at zero.
 CREATE TABLE players (
     id serial primary key,
     name text,
-    wins integer,
-    rounds integer
+    wins integer DEFAULT 0,
+    matches integer DEFAULT 0
 );
-
 -- matches table containing the round number, the teams competing ("home" and "away", with home being the team with more wins.)
 CREATE TABLE matches (
-    round integer,
-    home integer,
-    away integer,
-    winner integer,
+    match serial primary key,
+    winner integer references players(id),
+    loser integer references players(id)
 );
