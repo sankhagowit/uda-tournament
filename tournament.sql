a1a57ed3922d09fd9b74730a29b2a78890d519d5
Bobby <bobby@sankhagowit.com>
2017-05-05 13:55:56 -0500

    feat: Add matches table, removing match information from players table

--- tournament.sql
+++ tournament.sql
@@ -1,12 +1,21 @@
+-- Automate database creation
 -- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
 -- player table containing id, name, wins initialized at zero and matches
 -- initialized at zero.
+
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament
+
 CREATE TABLE players (
     id serial primary key,
     name text,
     wins integer DEFAULT 0,
     matches integer DEFAULT 0
+);
+
+CREATE TABLE matches (
+    match serial primary key,
+    winner integer references players (id),
+    loser integer references players (id),
 );
